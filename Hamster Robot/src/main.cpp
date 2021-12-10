994b41ca74d64c2215c5b6d6e346742fac0ba44e
JB_9304 <491617@student.fontys.nl>
2021-12-10 10:07:28 +0100

    Netjes maken
    code gedordend

--- Hamster Robot/src/main.cpp
+++ Hamster Robot/src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 #include <Servo.h>
+
 Servo motorLinks; 
 Servo motorRechts;
 int posR = 0;    // variable to store the servo position
 int posL = 0;
+
 const int echo = 3;
 const int trig = 5;
 long duration;
@@ -13,99 +15,109 @@ int rotate_array[359];
 int max_value = 0;
 int caldir = 0;
 boolean calibrating = false;
+
 const int LEDgeel = 4;
 const int LEDblauw = 12;
 const int lichtSensorEen = A0;
 const int lichtSensorVijf = A1;
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
+
   motorLinks.attach(2); 
   motorRechts.attach(13);
+  
   pinMode(LEDgeel, OUTPUT);
   pinMode(LEDblauw, OUTPUT);
   pinMode(lichtSensorEen, INPUT);
   pinMode(lichtSensorVijf, INPUT);
 }
-  void Right(){
-    motorRechts.write(90);
-    motorLinks.write(0);
-    return;
-  }
-  //laat de hamster naar rechts bewegen
-  void Left(){
-    motorRechts.write(0);
-    motorLinks.write(90);
-    return;
-  }
-  //laat de hamster naar links bewegen
-  void Straight(){
-    motorRechts.write(180);
-    motorLinks.write(0);
-    return;
-  }
-  // laat de hamster naar voren bewegen
-  void Distance(){
-    digitalWrite(trig,LOW);
-    delayMicroseconds(2);
+void Right()
+{
+  motorRechts.write(90);
+  motorLinks.write(0);
+  return;
+}
+//laat de hamster naar rechts bewegen
+void Left()
+{
+  motorRechts.write(0);
+  motorLinks.write(90);
+  return;
+}
+//laat de hamster naar links bewegen
+void Straight()
+{
+  motorRechts.write(180);
+  motorLinks.write(0);
+  return;
+}
+// laat de hamster naar voren bewegen
+void Distance()
+{
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
 
-    digitalWrite(trig,HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trig,LOW);
-    duration = pulseIn(echo,HIGH);
-    newdistance = duration*0.034/2;
-    return;
-    delay(50);
-  }
-  // neem de afstand waar door middel van een input en output sensor de sensoren werken door een soort echo locatie door middel van de newdistande formule wordt de afstand berekent
-   void Calibrate(){
-    int calcount = 0;
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  duration = pulseIn(echo,HIGH);
+  newdistance = duration*0.034/2;
+  return;
+  delay(50);
+}
+// neem de afstand waar door middel van een input en output sensor de sensoren werken door een soort echo locatie door middel van de newdistande formule wordt de afstand berekent
+void Calibrate()
+{
+  int calcount = 0;
 
-    while(calcount < 359)
-    {
-        motorLinks.write(360);
-        motorRechts.write(360);
-        Distance();
-        arraydistance = newdistance;
-        rotate_array[calcount] = arraydistance;
-        calcount++;
-
-        if(max_value < arraydistance)
-        {
-          max_value = arraydistance;
-          caldir = calcount;
-        }
-    }
-    if(calcount == 359)
-    {
-      motorRechts.write(caldir);
-      motorLinks.write(caldir);
-    }
-    // de calibreer methode maakt een array van 360 waarde's aan elke waarde wordt gevuld met een afstand als alle waarde zijn gevuld zal de hamster naar de verste waarde toe bewegen
-  }
-
-  void Backwards(){
+  while(calcount < 359)
+  {
+    motorLinks.write(360);
+    motorRechts.write(360);
     Distance();
-    Serial.println(newdistance);    
+    arraydistance = newdistance;
+    rotate_array[calcount] = arraydistance;
+    calcount++;
+
+    if(max_value < arraydistance)
+    {
+      max_value = arraydistance;
+      caldir = calcount;
+    }
+  }
+  if(calcount == 359)
+  {
+    motorRechts.write(caldir);
+    motorLinks.write(caldir);
+  }
+  // de calibreer methode maakt een array van 360 waarde's aan elke waarde wordt gevuld met een afstand als alle waarde zijn gevuld zal de hamster naar de verste waarde toe bewegen
+}
+
+void Backwards()
+{
+  Distance();
+  Serial.println(newdistance);    
+  motorRechts.write(0);
+  motorLinks.write(180);
+
+  while(newdistance < 20)
+  {
+    Distance();
     motorRechts.write(0);
     motorLinks.write(180);
-
-    while(newdistance < 20)
-    {
-      Distance();
-      motorRechts.write(0);
-      motorLinks.write(180);
-      Serial.println("waiting");
-      Serial.println(newdistance);
-      delay(100);
-    }
-
-    if(newdistance > 20)
-    {
-      Calibrate();
-    }   
-    //de achteruit methode haal altijd de nieuwe waarde va de afstand op als deze nog niet over de 20 is zal die achteruit blijven rijden als zit de afstand waarde hierboven wordt er gebruik gemaakt van de calibreer functie
+    Serial.println("waiting");
+    Serial.println(newdistance);
+    delay(100);
   }
+
+  if(newdistance > 20)
+  {
+    Calibrate();
+  }   
+  //de achteruit methode haal altijd de nieuwe waarde va de afstand op als deze nog niet over de 20 is zal die achteruit blijven rijden als zit de afstand waarde hierboven wordt er gebruik gemaakt van de calibreer functie
+}
 
 void loop() {
   //als bijde sensoren zwart zien gaat i vooruit
@@ -145,7 +157,7 @@ void loop() {
   digitalWrite(LEDgeel, LOW);
   digitalWrite(LEDblauw, LOW);
   */
-/*
+  /*
   digitalWrite(RGBgroen, HIGH);
   digitalWrite(RGBrood, HIGH);
   digitalWrite(RGBblauw, HIGH);
